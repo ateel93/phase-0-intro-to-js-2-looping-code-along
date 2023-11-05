67573b415f518d0cb6a8512cace888cb6c0936ea
Alexander <alex.teel93@gmail.com>
2023-11-05 16:19:35 -0500

    resubmit1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const nameCards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards() {
+  const thankYouMessages = [];
+  for (let i = 0; i < nameCards.length; i++) {
+    const message = `Thank you, ${nameCards[i]}, for the wonderful surprise gift!`;
+    thankYouMessages.push(message);
+  }
+  return thankYouMessages;
+}
+
+
+
+
